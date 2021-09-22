21aa102acf92bcdc4514fe47f481976f02a3f385
Sanket Naik <sanketnaik99@gmail.com>
2021-09-22 14:07:47 -0700

    Delete functionality added.

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,7 @@ export interface Student {
   firstNameNC?: string;
   lastNameNC?: string;
   sidSTR?: string;
+  docID?: string;
 }
 
 interface StudentRequest {
@@ -60,6 +61,11 @@ const Home: NextPage = () => {
         setStudents(data.data.students);
       }
     }
+  };
+
+  const deleteStudent = async (student: Student) => {
+    const data = await axios.post(`/api/delete-student`, { id: student.docID });
+    if (data.status == 200 && data.data.status == "SUCCESS") fetchStudents();
   };
 
   return (
@@ -190,6 +196,7 @@ const Home: NextPage = () => {
                   <th className="px-3 py-2">Email</th>
                   <th className="px-3 py-2">Mailing Address</th>
                   <th className="px-3 py-2">GPA</th>
+                  <th className="px-3 py-2">Action</th>
                 </tr>
               </thead>
               <tbody>
@@ -206,6 +213,37 @@ const Home: NextPage = () => {
                           {student.mailingAddress}
                         </td>
                         <td className="px-3 py-2 border">{student.gpa}</td>
+                        <td className="px-3 py-2 border">
+                          <div className="flex flex-row">
+                            <button className="bg-gray-300 text-gray-700 p-1.5 rounded-md mr-2">
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                              >
+                                <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                              </svg>
+                            </button>
+                            <button
+                              className="bg-red-500 text-white p-1.5 rounded-md"
+                              onClick={() => deleteStudent(student)}
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                              >
+                                <path
+                                  fillRule="evenodd"
+                                  d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                                  clipRule="evenodd"
+                                />
+                              </svg>
+                            </button>
+                          </div>
+                        </td>
                       </tr>
                     ))
                   : null}
