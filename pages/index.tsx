8d1e6398d0198d8b86766349b98de0a95dfe1f80
Sanket Naik <sanketnaik99@gmail.com>
2021-09-23 11:45:17 -0700

    Action button changes.

--- pages/index.tsx
+++ pages/index.tsx
@@ -215,7 +215,7 @@ const Home: NextPage = () => {
                         <td className="px-3 py-2 border">{student.gpa}</td>
                         <td className="px-3 py-2 border">
                           <div className="flex flex-row">
-                            <button className="bg-gray-300 text-gray-700 p-1.5 rounded-md mr-2">
+                            {/* <button className="bg-gray-300 text-gray-700 p-1.5 rounded-md mr-2">
                               <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 className="h-5 w-5"
@@ -224,7 +224,7 @@ const Home: NextPage = () => {
                               >
                                 <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
                               </svg>
-                            </button>
+                            </button> */}
                             <button
                               className="bg-red-500 text-white p-1.5 rounded-md"
                               onClick={() => deleteStudent(student)}
